48f0a2a8cd7f6732a079ac5c78a26a54c54935fd
Altheric <mauricedx@gmail.com>
2024-11-04 13:21:28 +0100

    Refactoring, gitignore

--- javascript/router.js
+++ javascript/router.js
@@ -1,20 +1,12 @@
 const queryLocation = window.location.search.substring(1);
-const nav = document.getElementsByClassName('nav-link')
-
-class HomePage extends HTMLElement {
-
-    async connectedCallback() {
-        let res = await fetch( 'pages/home.html' )
-
-        this.innerHTML = await res.text()
-    }
-
-}
-
-class AboutPage extends HTMLElement {
+const nav = document.getElementsByClassName('nav-link');
+//Valid locations to navigate to on site.
+const validLocations = ['home', 'about'];
+console.log(nav.namedItem())
+class CurrentPage extends HTMLElement {
 
     async connectedCallback() {
-        let res = await fetch( 'pages/about.html' )
+        let res = validLocations.includes(queryLocation) ? await fetch(`pages/${queryLocation}.html`) : await fetch( 'pages/home.html');
 
         this.innerHTML = await res.text()
     }
@@ -23,12 +15,12 @@ class AboutPage extends HTMLElement {
 
 switch(queryLocation){
     case 'about':
-        customElements.define( 'current-page', AboutPage )
+        customElements.define( 'current-page', CurrentPage )
         document.title = "Altheresy Postings: About";
         nav.namedItem('About').id='current-link';
         break;
     default:
-        customElements.define( 'current-page', HomePage )
+        customElements.define( 'current-page', CurrentPage )
         document.title = "Altheresy Postings: Home";
         nav.namedItem('Home').id='current-link';
         break;
