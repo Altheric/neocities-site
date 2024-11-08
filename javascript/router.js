a1ed387efec717444380f299bbae61108cb8707a
Altheric <mauricedx@gmail.com>
2024-11-08 14:59:28 +0100

    validlocname fix

--- javascript/router.js
+++ javascript/router.js
@@ -4,7 +4,6 @@ import { capitalize } from "./helpers.js";
 //Vars
 const queryLocation = window.location.search.substring(1);
 const nav = document.getElementsByClassName('nav-link');
-
 //Valid locations to navigate to on site.
 const validLocations = ['home', 'about'];
 const validLocName = validLocations.includes(queryLocation) ? capitalize(queryLocation) : 'Home';
@@ -12,7 +11,7 @@ const validLocName = validLocations.includes(queryLocation) ? capitalize(queryLo
 class CurrentPage extends HTMLElement {
 
     async connectedCallback() {
-        let res = await fetch(`pages/${validLocName.toLowerCase()}.html`);
+        let res = await fetch(`pages/${validLocations.toLowerCase()}.html`);
 
         this.innerHTML = await res.text()
     }
