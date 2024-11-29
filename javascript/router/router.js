5826bd7d0c49b90a45a5692544e5f90bbea5fc1c
Altheric <mauricedx@gmail.com>
2024-11-29 15:44:10 +0100

    Changed router to div append, not a custom element.

--- javascript/router/router.js
+++ javascript/router/router.js
@@ -1,16 +1,15 @@
 /**Display the current page of the querylocation */
 export const router = (queryLocation = '#home', routes) => {
     
-    class CurrentPage extends HTMLElement {
-
-        async connectedCallback() {
-            let res = await fetch(`pages/${routes[queryLocation].file}`);
-
-            this.innerHTML = await res.text()
-        }
-
-    }
-
-    customElements.define( 'current-page', CurrentPage )
+    getPage(queryLocation, routes);
     document.title = `Altheresy Postings: ${routes[queryLocation].title}`;
+    
+}
+/**Get the contents of the queried location. */
+const getPage = async (queryLocation = '#home', routes) => {
+    const content = document.getElementById('content')
+
+    let res = await fetch(`pages/${routes[queryLocation].file}`);
+    console.log(res)
+    content.innerHTML = await res.text()
 }
