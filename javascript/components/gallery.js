98572db1a89e167f6bd8186f61ea514c7e48ac24
Altheric <mauricedx@gmail.com>
2025-03-06 15:29:05 +0100

    focus image done, fix zoom in

--- javascript/components/gallery.js
+++ javascript/components/gallery.js
@@ -1,9 +1,24 @@
+let isShowing = false;
 
-/** Show a focused version of the image */
+/** Remove the focused image and make other elements clickable again. */
+const hide = (targetImage) => {
+    targetImage.remove()
+    isShowing = false;
+}
+
+/** Show a focused version of the image. */
 const show = (galleryImage) => {
-    const content = document.getElementById('page-content')
-    galleryImage.setAttribute('id', 'focused-image')
-    content.append(galleryImage)
+    const newImage = document.createElement('div');
+    newImage.append(galleryImage.cloneNode());
+    newImage.setAttribute('id', 'focused-image');
+    
+    isShowing = true;
+    
+    newImage.addEventListener('click', (event) => {
+        hide(newImage);
+    })
+
+    document.body.append(newImage);
 };
 
 /** Add event listener to the gallery clickables. */
@@ -12,7 +27,9 @@ export const galleryListener = () => {
     for (let index = 0; index < galleryItems.length; index++) {
         const item = galleryItems[index];
         item.addEventListener('click', (event) => {
-            show(galleryItems[index].children[0])
+            if(!isShowing){
+                show(galleryItems[index].children[0])
+            }
         })
     }
 }
